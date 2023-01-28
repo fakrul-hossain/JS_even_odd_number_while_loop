68388387deb77fcc6cca65dd9e9f96710ae68e28
fakrul-hossain <fakrulhossain912@gmial.com>
2023-01-28 17:09:15 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// even number using while loop
+// uncomment and watch the result in browser console
+console.log("this even number");
+let even = 0;
+
+while (even <= 10) {
+    console.log(even); 
+    even = even + 2
+}
+
+// odd number using while loop
+// uncomment and watch the result in browser console
+console.log("this odd number");
+let odd = 1;
+
+while (odd <= 10) {
+    console.log(odd);
+    odd = odd + 2
+}
+
+// reverse even number using while loop
+// uncomment and watch the result in browser console
+console.log("this is reverseEven number");
+let reverseEven = 10
+
+while (reverseEven >= 0) {
+    console.log(reverseEven);
+    reverseEven = reverseEven - 2
+}
+
+// reverse odd number using while loop
+// uncomment and watch the result in browser console
+console.log("this is reverseOdd number");
+
+let reverseOdd = 9 ;
+while (reverseOdd >= 0) {
+    console.log(reverseOdd);
+    reverseOdd = reverseOdd - 2
+}
